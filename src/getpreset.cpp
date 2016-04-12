d0880102acbf5da9a94aab509b86e60e3db6ccb2
Tamas Kenez <tamas.kenez@adasworks.com>
2016-04-12 23:23:49 +0200

    added tests for 3 main internal functions

--- src/getpreset.cpp
+++ src/getpreset.cpp
@@ -5,6 +5,7 @@
 
 #include <yaml-cpp/yaml.h>
 
+#include <adasworks/sx/check.h>
 #include <adasworks/sx/log.h>
 #include <adasworks/sx/string_par.h>
 #include <adasworks/sx/stringf.h>
@@ -73,6 +74,10 @@ void print_usage()
 The first argument is the the name or alias (alternative name) of the preset.
 The second argument is the name of the field to retrieve.
 
+Example (return the `args` fields of the `vs2013` preset):
+
+    getpreset vs2013 args
+
 The preset file will be loaded from the first valid location from the
 following list:
 
@@ -85,73 +90,171 @@ following list:
 )~");
 }
 
-vector<string> flatten(string_par input_path,
-                       string_par preset_name,
-                       string_par key,
-                       const YAML::Node n)
+// flatten a optionally nested sequence
+vector<string> flatten(const YAML::Node n)
 {
-    vector<string> vs;
-    for (auto& x : n) {
-        if (x.IsScalar())
-            vs.push_back(x.as<string>());
-        else if (n.IsSequence()) {
-            auto vsn = flatten(input_path, preset_name, key, x);
+    if (!n || n.IsNull())
+        return vector<string>();
+    if (n.IsScalar())
+        return vector<string>(1, n.as<string>());
+    if (n.IsSequence()) {
+        vector<string> vs;
+        for (auto& x : n) {
+            auto vsn = flatten(x);
             vs.insert(vs.end(), vsn.begin(), vsn.end());
-        } else {
-            fprintf(stderr, "The key \"%s\" in preset \"%s\" can't be flattened (\"%s\".\n",
-                    key.c_str(), preset_name.c_str(), input_path.c_str());
-            exit(EXIT_FAILURE);
         }
+        return vs;
     }
-    return vs;
+    throw std::runtime_error(
+        "Node can't be flattened because it contains a non-scalar, non-sequence node.");
 }
 
+void test_flatten()
+{
+    CHECK(flatten(YAML::Load("")) == vector<string>());
+    CHECK(flatten(YAML::Load("one")) == vector<string>({"one"}));
+    CHECK(flatten(YAML::Load("[one, two]")) == vector<string>({"one", "two"}));
+    CHECK(flatten(YAML::Load("[[one,three], two]")) == vector<string>({"one", "three", "two"}));
+    CHECK(flatten(YAML::Load("[one, [two, three]]")) == vector<string>({"one", "two", "three"}));
+    CHECK(flatten(YAML::Load("[one, [two, [three, four]]]")) ==
+          vector<string>({"one", "two", "three", "four"}));
+}
+
+// split x at nonquoted spaces
 vector<string> split(string_par x)
 {
     vector<string> vs;
     string s = x.str();
-    int start = 0;
     int N = s.size();
-    for (; start < N;) {
-        if (s[start] == ' ') {
-            ++start;
+    bool w_valid = false;
+    string w;
+    auto add_to_w = [&w, &w_valid](char c) {
+        if (!w_valid) {
+            w_valid = true;
+            w.clear();
+        }
+        w += c;
+    };
+    bool in_quote = false;
+    for (auto p = s.c_str(); *p; ++p) {
+        char c = *p;
+
+        // skip trailing spaces
+        if (!w_valid && *p == ' ') {
             continue;
         }
-        bool in_quote = false;
-        int end = start;
-        for (; end < N; ++end) {
-            if (s[end] == '"')
-                in_quote = !in_quote;
-            else if (s[end] == ' ') {
-                if (!in_quote) {
-                    vs.emplace_back(s.begin() + start, s.begin() + end);
-                    start = end + 1;
-                    break;
-                }
+
+        // handle escaped charactes: transmit backslash and next character
+        if (c == '\\') {
+            if (p[1] == 0) {
+                fprintf(stderr, "Single backslash at end of the string \"%s\".\n", s.c_str());
+                exit(EXIT_FAILURE);
             }
+            add_to_w('\\');
+            add_to_w(p[1]);
+            ++p;
+            continue;
         }
-        if (end == N) {
-            if (start < end) {
-                if (in_quote) {
-                    fprintf(stderr, "Unmatched double-quote in `%s`.", s.c_str());
-                    exit(EXIT_FAILURE);
-                }
-            }
-            vs.emplace_back(s.begin() + start, s.begin() + end);
-            break;
-        }
+
+        if (c == '"') {
+            in_quote = !in_quote;
+        } else if (c == ' ' && !in_quote) {
+            CHECK(w_valid && !w.empty());
+            vs.emplace_back(move(w));
+            w_valid = false;
+        } else
+            add_to_w(c);
+    }
+    if (in_quote) {
+        fprintf(stderr, "Unmatched double-quote in `%s`.", s.c_str());
+        exit(EXIT_FAILURE);
+    }
+    if (w_valid) {
+        CHECK(!w.empty());
+        vs.emplace_back(move(w));
     }
     return vs;
 }
 
+void test_split()
+{
+    CHECK(split("") == vector<string>());
+    CHECK(split("one") == vector<string>({"one"}));
+    CHECK(split("one two") == vector<string>({"one", "two"}));
+    CHECK(split("one     two") == vector<string>({"one", "two"}));
+    CHECK(split("one two three") == vector<string>({"one", "two", "three"}));
+    CHECK(split(" one two three") == vector<string>({"one", "two", "three"}));
+    CHECK(split(" one two three") == vector<string>({"one", "two", "three"}));
+    CHECK(split("  one two three") == vector<string>({"one", "two", "three"}));
+    CHECK(split("one    two    three ") == vector<string>({"one", "two", "three"}));
+    CHECK(split("one   two  three   ") == vector<string>({"one", "two", "three"}));
+    CHECK(split(" one two three ") == vector<string>({"one", "two", "three"}));
+    CHECK(split("   one two three   ") == vector<string>({"one", "two", "three"}));
+    CHECK(split("one \"two three\" four") == vector<string>({"one", "two three", "four"}));
+    CHECK(split("one \"two   three\" four") == vector<string>({"one", "two   three", "four"}));
+    CHECK(split("\"one two\" three four") == vector<string>({"one two", "three", "four"}));
+    CHECK(split(" \"one two\" three four") == vector<string>({"one two", "three", "four"}));
+    CHECK(split("  \"one two\" three four") == vector<string>({"one two", "three", "four"}));
+    CHECK(split("one two \"three four\"") == vector<string>({"one", "two", "three four"}));
+    CHECK(split("one two \"three four\" ") == vector<string>({"one", "two", "three four"}));
+    CHECK(split("one two \"three four\"  ") == vector<string>({"one", "two", "three four"}));
+    CHECK(split("\"one two\"") == vector<string>({"one two"}));
+    CHECK(split(" \"one two\"") == vector<string>({"one two"}));
+    CHECK(split("\"one two\" ") == vector<string>({"one two"}));
+    CHECK(split(" \"one two\" ") == vector<string>({"one two"}));
+    CHECK(split("  \"one two\"  ") == vector<string>({"one two"}));
+    CHECK(split(R"~(-Done=\"two\")~") == vector<string>({R"~(-Done=\"two\")~"}));
+}
+
 string subs(string_par x, string_par var, string_par value)
 {
-    string r;
+    if (var.empty()) {
+        fprintf(stderr, "A variable cannot be empty.\n");
+        exit(EXIT_FAILURE);
+    }
+    for (auto c : {'$', '{', '}'}) {
+        if (strchr(var.c_str(), c)) {
+            fprintf(stderr, "Character '%c' found in variable name \"%s\".\n", c, var.c_str());
+        }
+        if (strchr(value.c_str(), c)) {
+            fprintf(stderr, "Character '%c' found in variable value \"%s\".\n", c, value.c_str());
+        }
+    }
+    string r = x.str();
+    string s = string("${") + var.c_str() + "}";
+    for (;;) {
+        auto pos = r.find(s);
+        if (pos == string::npos)
+            break;
+        auto n = s.size();
+        r = r.substr(0, pos) + value.str() + r.substr(pos + n);
+    }
     return r;
+}
+
+void test_subs()
+{
+    CHECK(subs("${a} a ${c}", "a", "1") == "1 a ${c}");
+    CHECK(subs("${abc} a ${c}", "a", "1") == "${abc} a ${c}");
+    CHECK(subs("${abc} abc ${c}", "abc", "1") == "1 abc ${c}");
+    CHECK(subs("${abc} abc ${c}", "abc", "1") == "1 abc ${c}");
+    CHECK(subs("${abc} abc ${c}", "c", "2") == "${abc} abc 2");
+}
+
+void do_test()
+{
+    test_flatten();
+    test_split();
+    test_subs();
 }
 
 int main_core(int argc, char* argv[])
 {
+#if GETPRESET_DO_TEST
+    do_test();
+    return EXIT_SUCCESS;
+#endif
+
     if (argc != 3) {
         print_usage();
         exit(EXIT_FAILURE);
@@ -193,13 +296,14 @@ int main_core(int argc, char* argv[])
     }
 
     string preset_name;
+
     for (auto& n : presets) {
         string key = n.first.as<string>();
         if (key == lookup_name) {
             preset_name = key;
             break;
         }
-        auto aliases = flatten(input_path, preset_name, "alias", n.second["alias"]);
+        auto aliases = flatten(n.second["alias"]);
         for (auto& a : aliases) {
             if (a == lookup_name) {
                 preset_name = key;
@@ -223,7 +327,7 @@ int main_core(int argc, char* argv[])
     } else if (field == "args") {
         auto args = preset["args"];
         if (args) {
-            auto v = flatten(input_path, preset_name, "args", args);
+            auto v = flatten(args);
             vector<string> vv;
             for (auto& s : v) {
                 auto ss = split(s);
